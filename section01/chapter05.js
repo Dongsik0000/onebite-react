f90b890e4172316db37536662c7b7527b09961eb
Dongsik0000 <panda1842@naver.com>
2025-03-19 01:20:49 +0900

    refactor: react 1일차

---
+++ section01/chapter05.js
@@ -0,0 +1,16 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+console.log(introduce);
